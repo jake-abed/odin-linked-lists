3ba75b4cd475708a9f8f481119f8c728385a2fbc
Jake Abed <jakeabed@gmail.com>
2023-09-25 21:37:50 -0700

    Create basic LinkedList class with getters and methods

--- linkedLists.js
+++ linkedLists.js
@@ -3,13 +3,79 @@
 //LinkedList class or factory - This is the full list
 //with all attached methods
 
+class LinkedList {
+    constructor(head = null) {
+        this.head = new Node(head);
+    }
+
+    get tail() {
+        let current = this.head;
+        let next = current.nextNode;
+
+        while (next) {
+                current = next;
+                next = current.nextNode;
+            }
+        return current;
+    }
+
+    get size() {
+        let i = 0;
+        let current = this.head;
+        while (current.nextNode) {
+            current = current.nextNode;
+            i++;
+        }
+        return i;
+    }
+
+    #removeTail() {
+        let current = this.head;
+        let next = current.nextNode;
+
+        while (next.nextNode) { 
+                current = next;
+                next = current.nextNode;
+            }
+        return current.nextNode = null
+    }
+
+    pop () {
+        let tail = this.tail;
+        this.#removeTail();
+        return tail;
+    }
+
+    append(value) {
+        return this.tail.nextNode = new Node(value);
+    }
+
+    prepend(value) {
+        let valueNode = new Node(value, this.head);
+        return this.head = valueNode;
+    }
+
+}
+
 //Node class or factory with value property,
 //nextNode property (null if not pointing elsewhere)
 
-function Node(value, nextNode = 'null') {
-    return { value, nextNode };
+class Node {
+    constructor(value, nextNode = null) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
 }
 
-const testNode = Node({dog: 'Woof', cat: 'Meow'});
+const ll1 = new LinkedList('monkey');
 
-console.log(testNode);
+console.log(ll1);
+
+ll1.prepend('cat');
+
+ll1.append('dog');
+ll1.append('chicken');
+ll1.prepend('human');
+
+console.log(ll1);
+console.log(ll1.tail);
